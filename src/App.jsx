8a2887cbccc11ff769ba47978a3cb83b2453bf69
Amrin Reihana <amrins3006@gmail.com>
2025-01-16 11:49:05 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+
+
+
 import React from 'react';
 import Navbar from './components/Navbar';
 import Result from './components/Result';
@@ -9,40 +12,60 @@ import Suggestions from './components/Suggestion';
 import AccuracyChart from './components/AccuracyChart';
 import TimeTaken from './components/TimeTaken';
 
+
 const App = () => {
   return (
-    <div className="bg-gray-100 min-h-screen flex flex-col">
+    <div className="bg-gray-100 min-h-screen">
       {/* Navbar */}
       <Navbar />
 
       {/* Main Section */}
-      <main className="p-4 sm:p-6 lg:p-8 flex justify-center">
-        <div className="flex flex-col gap-6 w-full lg:w-[90%]">
-          {/* First Row: Result */}
-          <div className="w-full lg:w-1/4 self-center lg:self-start">
+      <main className="p-4 sm:p-8 flex justify-center">
+        <div className="flex flex-col sm:flex-row gap-6 w-full sm:w-[90%]">
+          {/* Left Column: Result */}
+          <div className="w-full sm:w-1/4">
             <Result />
           </div>
 
-          {/* Second Row: Compare */}
-          <div className="w-full">
+          {/* Right Column: Compare, Improvements, Response, Approach, Suggestions, AccuracyChart, and TimeTaken */}
+          <div className="w-full sm:w-3/4 flex flex-col gap-6">
+            {/* Compare Section */}
             <Compare />
-          </div>
 
-          {/* Third Row: Improvements, Response, Approach, and Suggestions */}
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
-            <Improvements />
-            <Response />
-            <Approach />
-            <Suggestions />
-          </div>
+            {/* Row: Improvements, Response, Approach, and Suggestions */}
+            <div className="grid grid-cols-1 sm:grid-cols-4 gap-6">
+              {/* Improvements */}
+              <div className=" rounded-lg">
+                <Improvements />
+              </div>
 
-          {/* Fourth Row: AccuracyChart and TimeTaken */}
-          <div className="flex flex-col lg:flex-row gap-4">
-            <div className="flex-1">
-              <AccuracyChart />
+              {/* Response */}
+              <div className=" rounded-lg">
+                <Response />
+              </div>
+
+              {/* Approach */}
+              <div className=" rounded-lg">
+                <Approach />
+              </div>
+
+              {/* Suggestions */}
+              <div className=" rounded-lg">
+                <Suggestions />
+              </div>
             </div>
-            <div className="flex-1">
-              <TimeTaken />
+
+            {/* Row: AccuracyChart and TimeTaken */}
+            <div className="flex flex-col sm:flex-row justify-between items-stretch gap-6">
+              {/* AccuracyChart */}
+              <div className="flex-1   rounded-lg">
+                <AccuracyChart />
+              </div>
+
+              {/* TimeTaken */}
+              <div className="flex-1   rounded-lg">
+                <TimeTaken />
+              </div>
             </div>
           </div>
         </div>
